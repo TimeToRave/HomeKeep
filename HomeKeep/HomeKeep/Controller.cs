54c46e35d3a95a7e92667e95dfcaffc08586960e
kwenda <nickkuzmin1301@gmail.com>
2018-11-29 16:47:01 +0300

    Перевод приложения MS SQL на SQLite

--- HomeKeep/HomeKeep/Controller.cs
+++ HomeKeep/HomeKeep/Controller.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Data.Common;
 
 namespace HomeKeep
 {
@@ -11,27 +13,26 @@ namespace HomeKeep
     static public class Controller
     {
 
-
         static private string RunSqlQuery(string query)
         {
-            String connectionString = @"Data Source = DESKTOP-M37KRRP\SQL2017;Initial Catalog=Notes;User ID=sa;Password=11223344";
-            SqlConnection connection = new SqlConnection(connectionString);
+            String connectionString = @"Data Source=" + AppDomain.CurrentDomain.BaseDirectory.ToString() + "Note.db";
+            SQLiteConnection connection = new SQLiteConnection(connectionString);
 
             connection.Open();
 
-            SqlCommand command;
-            SqlDataReader dataReader;
-            
-            command = new SqlCommand(query, connection);
+            SQLiteCommand command;
+            SQLiteDataReader dataReader;
+
+            command = new SQLiteCommand(query, connection);
             dataReader = command.ExecuteReader();
 
             string result = "";
             int count = dataReader.FieldCount;
-            while (dataReader.Read())
+            foreach (DbDataRecord record in dataReader)
             {
                 for (int i = 0; i < count; i++)
                 {
-                    result += dataReader.GetValue(i).ToString()+ "<#>";
+                    result += record[i].ToString() + "<#>";
                 }
                 result += "<@>";
             }
@@ -42,7 +43,9 @@ namespace HomeKeep
             return result;
 
         }
-        
+
+
+
         /// <summary>
         /// Метод, запрашивающий у БД все заметки
         /// </summary>
@@ -75,35 +78,30 @@ namespace HomeKeep
         /// <param name="note">Экземпляр класса Note</param>
         static public void CreateNote(Note note)
         {
-            string query = "INSERT INTO Note VALUES ('" + note.Name + "', '" + note.Text + "', " + note.Tag.ToString() + " )";
+            string query = "INSERT INTO Note (\"name\", \"text\", \"tag\") VALUES ('" + note.Name + "', '" + note.Text + "', " + note.Tag.ToString() + " );";
             RunSqlQuery(query);
         }
 
         static public void CreateTag(Tag tag)
         {
-            string query = "INSERT INTO Tag VALUES ('" + tag.Name + "' )";
+            string query = "INSERT INTO Tag (\"name\") VALUES ('" + tag.Name + "' );";
             RunSqlQuery(query);
         }
 
 
         static public void DeleteNote(int id)
         {
-            string query = "DELETE FROM Note Where id = " + id.ToString();
+            string query = "DELETE FROM Note Where id = " + id.ToString() + ";";
             RunSqlQuery(query);
         }
 
         static public void DeleteTag(int id)
         {
-            string query = "DELETE FROM Tag Where id = " + id.ToString();
+            string query = "PRAGMA foreign_keys = \"1\"; DELETE FROM Tag WHERE id = " + id.ToString() + ";";
             RunSqlQuery(query);
         }
 
-        static public void DeleteNote(Note note)
-        {
-            string query = "DELETE FROM Note Where id = " + note.Id;
-            RunSqlQuery(query);
-        }
-        
+       
         /// <summary>
         /// Метод, запрашивающий у БД все заметки
         /// </summary>
@@ -111,7 +109,7 @@ namespace HomeKeep
         static public Tag[] GetAllTags()
         {
             String result = "";
-            String query = "SELECT * FROM Tag";
+            String query = "SELECT * FROM Tag;";
 
             result = RunSqlQuery(query);
 
@@ -134,7 +132,7 @@ namespace HomeKeep
         static public string GetTagName(int id)
         {
             string result = "";
-            string query = "SELECT name FROM Tag WHERE id=" + id.ToString();
+            string query = "SELECT name FROM Tag WHERE id=" + id.ToString() + ";";
 
             result = RunSqlQuery(query);
 
@@ -152,7 +150,7 @@ namespace HomeKeep
         public static Note [] SearchByTag (int tagId)
         {
             String result = "";
-            String query = "SELECT * FROM Note WHERE Note.tag=" + tagId.ToString() + "ORDER BY id DESC";
+            String query = "SELECT * FROM Note WHERE tag=" + tagId.ToString() + " ORDER BY id DESC;";
 
             result = RunSqlQuery(query);
 
@@ -171,20 +169,20 @@ namespace HomeKeep
 
         public static void EditNote (Note note)
         {
-            string query = "UPDATE Note SET name='" + note.Name + "' , text ='" + note.Text + "', tag='" + note.Tag.ToString() + "' WHERE id = " + note.Id.ToString();
+            string query = "UPDATE Note SET name='" + note.Name + "' , text ='" + note.Text + "', tag='" + note.Tag.ToString() + "' WHERE id = " + note.Id.ToString() + ";";
             RunSqlQuery(query);
         }
 
         public static void EditTag(Tag tag)
         {
-            string query = "UPDATE Tag SET name='" + tag.Name + "' WHERE id = " + tag.Id.ToString();
+            string query = "UPDATE Tag SET name='" + tag.Name + "' WHERE id = " + tag.Id.ToString() + ";";
             RunSqlQuery(query);
         }
 
         public static Note GetNote (int id)
         {
             String result = "";
-            String query = "SELECT * FROM Note WHERE id=" + id.ToString();
+            String query = "SELECT * FROM Note WHERE id=" + id.ToString() + ";";
 
             result = RunSqlQuery(query);
             
@@ -206,7 +204,7 @@ namespace HomeKeep
         public static Tag GetTag(int id)
         {
             String result = "";
-            String query = "SELECT * FROM Tag WHERE id=" + id.ToString();
+            String query = "SELECT * FROM Tag WHERE id=" + id.ToString() + ";";
 
             result = RunSqlQuery(query);
 
